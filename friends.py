79fd58a8d330553b3f4cb3476e85103d68a7e192
Nadia Ballarta Uchuya <nadiaballartauchuya@Nadias-MacBook-Air.local>
2023-11-25 18:53:23 +0000

    Initial commit

---
+++ friends.py
@@ -0,0 +1,29 @@
+
+
+# Exercise 2.2: You have friends at your house for dinner and you've
+# accidentally burnt the lasagne. Time to order pizza.
+# Write a program calculate how many pizzas you need to feed you
+# and your friends
+
+# friends = input('how many friends do you have at your house?')
+# pizzas_to_order = int(friends) * 1
+#
+#
+# print( 'my friends and I will order {} pizzas '.format(pizzas_to_order))
+age = range(1,100)
+
+
+
+max_age = 100
+age = 10
+
+# for number in age:
+#     print(number)
+
+while age < max_age:
+    age = age + 1
+
+    if age == 100:
+        print('Nadia bonita')
+    else:
+        print('Nadia fea', age)
---
+++ input.py
@@ -0,0 +1,26 @@
+# #
+# # nombre = input('Cual es tu nombre? ')
+# edad = input('cual es tu edad? ')
+#
+# print(  int(edad) + (2)   )
+#
+# # # if nombre == ('nadia') and int(edad) > 18:
+# # #     print('Eres la mejor de todas amor {}'.format(nombre))
+# # # else:
+# # #     print('tu eres feo')
+# # #
+# #
+#
+#
+# Exercise 2.1: Write a program that asks two questions using input()
+# then prints the values that were entered. You can choose any questions
+# that you want.
+# fruit = input('What fruit do you like? ')
+# veg = input('What veg do you like? ')
+# print('You like {} and you like {}'.format(veg, fruit))
+
+purchased_apples = input('How many apples did you buy? ')
+print(type(True))
+
+
+
